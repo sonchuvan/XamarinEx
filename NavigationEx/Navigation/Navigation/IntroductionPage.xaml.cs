12ee4e306125e28c6a1ff3d488f21a71182930a3
Your Name <soncv97@gmail.com>
2020-09-04 13:50:03 +0700

    a1

---
+++ NavigationEx/Navigation/Navigation/IntroductionPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Navigation
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class IntroductionPage : ContentPage
+    {
+        public IntroductionPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PopAsync();
+        }
+    }
+}
